ba86f91496c019222e96c878038e32b4074e6ff2
sebastianellingsen <sebastian.ellingsen@upf.edu>
2018-12-31 18:55:24 +0000

    loading dataset

--- missing_data.R
+++ missing_data.R
@@ -1,1 +1,13 @@
 # This note presents how to deal with missing data
+
+# Missing at random
+cars <- mtcars %>% mutate(mpg = ifelse(runif(32)>0.7, NA, mpg),
+                          hp = ifelse(runif(32)>0.9, NA, hp),
+                          wt = ifelse(runif(32)>0.85, NA, wt))
+
+## Detecting missing data
+
+# Let's summarize the data to look for missing values using stargazer:
+cars %>% as.data.frame %>% stargazer(type="text")
+
+# Base R has several features to detect missing data
